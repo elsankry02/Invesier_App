070db9f1fab5f56e803b09b3f74ab3d58e3176ff
elsankry02 <githup800900@gmail.com>
2025-05-13 06:51:04 +0300

    Refactor CustomPrimaryButton to accept onTap callback and textColor parameter

--- lib/components/custom_primary_button.dart
+++ lib/components/custom_primary_button.dart
@@ -2,22 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:invesier/constant/color_manger.dart';
 
 class CustomPrimaryButton extends StatelessWidget {
-  const CustomPrimaryButton({super.key});
-
+  const CustomPrimaryButton({super.key, this.onTap, required this.textColor});
+  final void Function()? onTap;
+  final Color textColor;
   @override
   Widget build(BuildContext context) {
     final kText = Theme.of(context).textTheme;
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(60),
-        gradient: LinearGradient(
-          colors: [ColorManger.kEucalyptus, ColorManger.kTurquoiseBlue],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        padding: EdgeInsets.symmetric(vertical: 11),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(60),
+          gradient: LinearGradient(
+            colors: [ColorManger.kEucalyptus, ColorManger.kTurquoiseBlue],
+          ),
         ),
-      ),
-      child: Center(
-        child: Text(
-          'Next',
-          style: kText.titleMedium!.copyWith(fontWeight: FontWeight.w600),
+        child: Center(
+          child: Text(
+            'Next',
+            style: kText.titleMedium!.copyWith(
+              fontWeight: FontWeight.w600,
+              color: textColor,
+            ),
+          ),
         ),
       ),
     );
