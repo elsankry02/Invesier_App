84b5a7039951fd05258eb8b6bba2b42b69af2963
elsankry02 <githup800900@gmail.com>
2025-05-13 05:13:34 +0300

    Refactor font assets and update onboarding page
    - Removed Lato font files and added Manrope font files to the project.
    - Updated pubspec.yaml to reflect the new font family and assets.
    - Created a new onboarding page with a basic scaffold structure.
    - Added a string manager for font family constants.

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:invesier/constant/string_manger.dart';
 
 class Invesier extends StatelessWidget {
   const Invesier({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(theme: ThemeData(fontFamily: StringManger.kManrope));
   }
 }
