578905c684d708d9121de6414bd44ccb84196266
elsankry02 <githup800900@gmail.com>
2025-05-13 14:46:02 +0300

    Add SVG assets and update image and SVG managers for new resources

--- lib/constant/image_manger.dart
+++ lib/constant/image_manger.dart
@@ -1,1 +1,3 @@
-class ImageManger {}
+class ImageManger {
+  static const kLogIn = 'assets/image/Log_In.png';
+}
--- lib/constant/svg_manger.dart
+++ lib/constant/svg_manger.dart
@@ -1,1 +1,5 @@
-class SvgManger {}
+class SvgManger {
+  static const kLogo = 'assets/svg/Logo.svg';
+  static const kSvgexport = 'assets/svg/svgexport-15.svg';
+  static const kGroup = 'assets/svg/Group.svg';
+}
