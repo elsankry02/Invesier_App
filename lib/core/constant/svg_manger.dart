de25ce480a5a95709f43aa89c1296fe46d686778
elsankry02 <githup800900@gmail.com>
2025-06-19 16:52:56 +0300

    feat: Add new SVG assets for comment, sharing, growth, decline, and delete; update SVG manager

--- lib/core/constant/svg_manger.dart
+++ lib/core/constant/svg_manger.dart
@@ -7,4 +7,9 @@ class SvgManger {
   static const kChallenges = 'assets/svg/challenges.svg';
   static const kTrade = 'assets/svg/trade.svg';
   static const kLearn = 'assets/svg/frame.svg';
+  static const kComment = 'assets/svg/comment.svg';
+  static const kSharing = 'assets/svg/sharing.svg';
+  static const kGrowth = 'assets/svg/growth.svg';
+  static const kDecline = 'assets/svg/decline.svg';
+  static const kDelete = 'assets/svg/delete.svg';
 }
