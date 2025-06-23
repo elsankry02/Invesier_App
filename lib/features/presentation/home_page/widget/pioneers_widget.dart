d343ef4cac1d290d2326efa9193f29a38b5b8428
elsankry02 <githup800900@gmail.com>
2025-06-23 14:26:05 +0300

    feat: Add PioneersWidget as a new stateless widget for the home page

---
+++ lib/features/presentation/home_page/widget/pioneers_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PioneersWidget extends StatelessWidget {
+  const PioneersWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: []);
+  }
+}
