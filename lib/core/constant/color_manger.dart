83ce9831bdb0391039ed4de12023c1bb1de33f2c
elsankry02 <githup800900@gmail.com>
2025-06-29 13:39:53 +0300

    refactor: Rearrange color constants in ColorManger for improved organization

--- lib/core/constant/color_manger.dart
+++ lib/core/constant/color_manger.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ColorManger {
+  static const k1 = Color(0xff222B25);
+  static const k2 = Color(0xff141916);
+  static const kRed = Color(0xffDC4453);
   static const kGray = Color(0xff818181);
   static const kWhite = Color(0xffFFFFFF);
   static const kBorder = Color(0xffD0D2D8);
+  static const kCodGray = Color(0xff1E1E1E);
   static const kPrimary = Color(0xff44A570);
   static const kBoulder = Color(0xff7b7b7b);
-  static const kCodGray = Color(0xff1E1E1E);
   static const kTertiary = Color(0xff171B1A);
   static const kSecondary = Color(0xffB9A435);
   static const kBackGround = Color(0xff171717);
   static const kHeavyMetal = Color(0xff222B25);
   static const kPersianRed = Color(0xffD32F2F);
-  static const kRed = Color(0xffDC4453);
   static const kOceanGreen = Color(0xff41af8b);
   static const kEucalyptus = Color(0xff6AEEC6);
   static const kTurquoiseBlue = Color(0xff248866);
-  static const k1 = Color(0xff222B25);
-  static const k2 = Color(0xff141916);
 }
