80799395081c9b1d84fc1a2db0c93d9a56c7c92c
elsankry02 <githup800900@gmail.com>
2025-06-30 11:56:29 +0300

    refactor: Remove unused image picker functionality and clean up code in CustomShowModelButtonAddCommentWidget

--- lib/core/components/custom_show_model_button_add_comment_widget.dart
+++ lib/core/components/custom_show_model_button_add_comment_widget.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:invesier/core/components/custom_button_widget.dart';
 import 'package:invesier/core/constant/color_manger.dart';
 import 'package:invesier/core/constant/svg_manger.dart';
@@ -31,35 +28,7 @@ class CustomShowModelButtonAddCommentWidget extends StatefulWidget {
 
 class _CustomShowModelButtonAddCommentWidgetState
     extends State<CustomShowModelButtonAddCommentWidget> {
-  File? file;
   final commentController = TextEditingController();
-  @override
-  void dispose() {
-    commentController.dispose();
-    super.dispose();
-  }
-
-  imageGallery() async {
-    final imageGallery = await ImagePicker().pickImage(
-      source: ImageSource.gallery,
-    );
-    if (imageGallery == null) return;
-
-    setState(() {
-      file = File(imageGallery.path);
-    });
-  }
-
-  imagecamera() async {
-    final imagecamera = await ImagePicker().pickImage(
-      source: ImageSource.camera,
-    );
-    if (imagecamera == null) return;
-
-    setState(() {
-      file = File(imagecamera.path);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,64 +46,62 @@ class _CustomShowModelButtonAddCommentWidgetState
         padding: EdgeInsets.only(
           bottom: MediaQuery.of(context).viewInsets.bottom,
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              //! kArrow
-              SvgPicture.asset(SvgManger.kArrow),
-              SizedBox(height: context.height * 0.004),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //! titel
-                  Text(
-                    widget.title,
-                    style: context.kTextTheme.titleSmall!.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            //! kArrow
+            SvgPicture.asset(SvgManger.kArrow),
+            SizedBox(height: context.height * 0.004),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //! titel
+                Text(
+                  widget.title,
+                  style: context.kTextTheme.titleSmall!.copyWith(
+                    fontWeight: FontWeight.w600,
                   ),
-                  //! icon
-                  IconButton(
-                    onPressed: () {
-                      context.router.maybePop();
-                    },
-                    icon: Icon(FontAwesomeIcons.xmark),
-                  ),
-                ],
-              ),
-              //! TextFormField
-              TextFormField(
-                cursorColor: ColorManger.kWhite,
-                controller: commentController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.zero,
-                  hintText: widget.hintText,
-                  hintStyle: TextStyle(
-                    color: ColorManger.kGray,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  border: OutlineInputBorder(borderSide: BorderSide.none),
                 ),
+                //! icon
+                IconButton(
+                  onPressed: () {
+                    context.router.maybePop();
+                  },
+                  icon: Icon(FontAwesomeIcons.xmark),
+                ),
+              ],
+            ),
+            //! TextFormField
+            TextFormField(
+              cursorColor: ColorManger.kWhite,
+              controller: commentController,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
+                hintText: widget.hintText,
+                hintStyle: TextStyle(
+                  color: ColorManger.kGray,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+                border: OutlineInputBorder(borderSide: BorderSide.none),
               ),
+            ),
 
-              SizedBox(height: context.height * 0.012),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  //! titleButton
-                  CustomButtonWidget(
-                    title: widget.titleButton,
-                    onTap: () {},
-                    vertical: widget.vertical,
-                    radius: widget.radius,
-                  ),
-                ],
-              ),
-              SizedBox(height: context.height * 0.012),
-            ],
-          ),
+            SizedBox(height: context.height * 0.012),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                //! titleButton
+                CustomButtonWidget(
+                  title: widget.titleButton,
+                  onTap: () {},
+                  vertical: widget.vertical,
+                  radius: widget.radius,
+                ),
+              ],
+            ),
+            SizedBox(height: context.height * 0.012),
+          ],
         ),
       ),
     );
