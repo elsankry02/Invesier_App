c66917274647afc604c1c06a48b591732b594f7e
elsankry02 <githup800900@gmail.com>
2025-07-06 13:07:03 +0300

    feat: Add NotificationStartedFollowingYouWidget for user follow notifications

--- lib/core/constant/color_manger.dart
+++ lib/core/constant/color_manger.dart
@@ -6,6 +6,7 @@ class ColorManger {
   static const kRed = Color(0xffDC4453);
   static const kGray = Color(0xff818181);
   static const kWhite = Color(0xffFFFFFF);
+  static const kRedTwo = Color(0xffD32F2F);
   static const kBorder = Color(0xffD0D2D8);
   static const kDivider = Color(0xff2F2F2F);
   static const kCodGray = Color(0xff1E1E1E);
@@ -15,9 +16,8 @@ class ColorManger {
   static const kSecondary = Color(0xffB9A435);
   static const kBackGround = Color(0xff171717);
   static const kHeavyMetal = Color(0xff222B25);
-  static const kRedTwo = Color(0xffD32F2F);
   static const kOceanGreen = Color(0xff41af8b);
   static const kEucalyptus = Color(0xff6AEEC6);
-  static const kTurquoiseBlue = Color(0xff248866);
   static const kDarkenText = Color(0xff687684);
+  static const kTurquoiseBlue = Color(0xff248866);
 }
---
+++ lib/features/presentation/home/notification_page/widget/notification_started_following_you_widget.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:invesier/core/components/custom_button_widget.dart';
+import 'package:invesier/core/constant/color_manger.dart';
+import 'package:invesier/core/constant/image_manger.dart';
+import 'package:invesier/core/extension/extension.dart';
+
+class NotificationStartedFollowingYouWidget extends StatelessWidget {
+  const NotificationStartedFollowingYouWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsetsDirectional.symmetric(horizontal: 20, vertical: 12),
+      decoration: BoxDecoration(
+        color: ColorManger.k1,
+        borderRadius: BorderRadiusDirectional.circular(13),
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        // image
+        leading: ClipOval(
+          child: Image.asset(
+            ImageManger.kBoyThree,
+            height: 45,
+            width: 45,
+            fit: BoxFit.cover,
+          ),
+        ),
+        // title
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Charlie White',
+                  style: context.kTextTheme.titleSmall!.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  '@charlie_w',
+                  style: context.kTextTheme.titleSmall!.copyWith(
+                    fontWeight: FontWeight.w400,
+                    color: ColorManger.kDarkenText,
+                  ),
+                ),
+              ],
+            ),
+            // Chase Custom Button Widget
+            CustomButtonWidget(
+              title: 'Chase',
+              titleColor: ColorManger.kWhite,
+              backGroundColor: ColorManger.kTurquoiseBlue,
+              borderColor: ColorManger.kTurquoiseBlue,
+              radius: 31,
+              vertical: 4,
+              horizontal: 20,
+            ),
+          ],
+        ),
+        // subtitle
+        subtitle: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'started following you.',
+              style: context.kTextTheme.titleMedium!.copyWith(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Text(
+              '2 hours ago',
+              style: context.kTextTheme.labelMedium!.copyWith(
+                fontWeight: FontWeight.w400,
+                color: ColorManger.kDarkenText,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
