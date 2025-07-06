b01fc3a6f8be1df90e449bb137731c9e4f6b3a98
elsankry02 <githup800900@gmail.com>
2025-07-06 13:07:12 +0300

    feat: Implement NotificationAppBarWidget for enhanced navigation in notifications

---
+++ lib/features/presentation/home/notification_page/widget/notification_appbar_widget.dart
@@ -0,0 +1,35 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:invesier/core/constant/color_manger.dart';
+import 'package:invesier/core/extension/extension.dart';
+
+class NotificationAppBarWidget extends StatelessWidget {
+  const NotificationAppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        //! icon
+        IconButton(
+          onPressed: () {
+            context.router.maybePop();
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: ColorManger.kWhite,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            textAlign: TextAlign.center,
+            'Notification',
+            style: context.kTextTheme.titleLarge!.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
